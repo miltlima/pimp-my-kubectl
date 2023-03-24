48d304259cf37a38d39fa375c7ebebd5c8317b91
Milton Jesus <milton.lima@outlook.com>
2023-03-24 14:51:50 -0300

    remove verbose output

--- pimp-my-kubectl.sh
+++ pimp-my-kubectl.sh
@@ -25,12 +25,12 @@ if command -v kubectx &>/dev/null; then
     echo "kubectx is installed"
 else
     if [ "$OS" == "linux" ]; then
-        curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz
-        tar -zxvf kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz
-        sudo cp kubectx /usr/local/bin/
-        sudo chmod +x /usr/local/bin/kubectx
-        export PATH="/usr/local/bin:$PATH"
-        rm -rf kubectx kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz
+        curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
+        tar -zxvf kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
+        sudo cp kubectx /usr/local/bin/ && spinner &
+        sudo chmod +x /usr/local/bin/kubectx && spinner &
+        export PATH="/usr/local/bin:$PATH" && spinner &
+        rm -rf kubectx kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
     elif [ "$os" == "darwin" ]; then
         brew install kubectx
     else
@@ -40,15 +40,14 @@ fi
 
 # Install Krew
 (
-    set -x
-    cd "$(mktemp -d)" &&
-        OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-        ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-        KREW="krew-${OS}_${ARCH}" &&
-        curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-        tar zxvf "${KREW}.tar.gz" &&
-        ./"${KREW}" install krew
-)
+    set -x; cd "$(mktemp -d)" &&
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+    KREW="krew-${OS}_${ARCH}" &&
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+    tar zxvf "${KREW}.tar.gz" &&
+    ./"${KREW}" install krew &
+) > /dev/null 2>&1
 
 shell=$(basename "$SHELL")
 rc_file="$HOME/.${shell}rc"
@@ -58,7 +57,7 @@ rc_file="$HOME/.${shell}rc"
 if grep -q 'alias k=' ~/.bashrc || grep -q 'alias k=' ~/.zshrc; then
     echo "Alias k exists"
 else
-    echo "alias k=kubectl" >>"$rc_file"
+    echo "alias k=kubectl" >>"$(rc_file)"
 fi
 
 if grep -q 'alias kx=' ~/.bashrc || grep -q 'alias kx=' ~/.zshrc; then
@@ -68,13 +67,13 @@ else
 fi
 
 if grep -q 'PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' ~/.bashrc || grep -q 'PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' ~/.zshrc; then
-    echo "The PATH exists in either ~/.bashrc or ~/.zshrc"
+    echo "The PATH already exists in either ~/.bashrc or ~/.zshrc" > /dev/null 2>&1
 else
     echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >>"$rc_file"
 fi
 
 # Source the file to make the changes take effect
-source "$rc_file"
+source "$rc_file" > /dev/null 2>&1
 
 # Install krew plugins
 PLUGINS=(
@@ -89,5 +88,11 @@ PLUGINS=(
 )
 
 for plugin in "${PLUGINS[@]}"; do
+    echo "Installing plugin: $plugin"
+    if ! kubectl krew list | grep -q "$plugin"; then
     kubectl krew install "$plugin"
+    echo "Plugin $plugin installed successfully."
+    else
+    echo "Plugin $plugin is already installed."
+    fi
 done
