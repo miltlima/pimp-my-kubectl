f0e0e0de7a48e0fc1c47d1e0f4e44ee2761a1511
Milton Jesus <milton.lima@outlook.com>
2023-04-27 19:51:00 -0300

    improve code

--- pimp-my-kubectl.sh
+++ pimp-my-kubectl.sh
@@ -8,91 +8,87 @@ ARCH=$(uname -m)
 
 # Install Kubectl
 if [ -x "$(command -v kubectl)" ]; then
-    echo "kubectl already installed"
+  echo "kubectl already installed"
 else
-    case "$OS" in
-    linux) curl -LO https://dl.k8s.io/release/$VERSION/bin/linux/$ARCH/kubectl ;;
-    darwin) curl -LO https://dl.k8s.io/release/$VERSION/bin/darwin/$ARCH/kubectl ;;
-    *)
-        echo "Unsupported OS: $OS"
-        exit 1
-        ;;
-    esac
+  case "$OS" in
+  linux) curl -LO https://dl.k8s.io/release/$VERSION/bin/linux/$ARCH/kubectl ;;
+  darwin) curl -LO https://dl.k8s.io/release/$VERSION/bin/darwin/$ARCH/kubectl ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+  esac
 fi
 
 # Install kubectx
 if command -v kubectx &>/dev/null; then
-    echo "kubectx is installed"
+  echo "kubectx is already installed"
 else
-    if [ "$OS" == "linux" ]; then
-        curl -LO https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
-        tar -zxvf kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
-        sudo cp kubectx /usr/local/bin/ && spinner &
-        sudo chmod +x /usr/local/bin/kubectx && spinner &
-        export PATH="/usr/local/bin:$PATH" && spinner &
-        rm -rf kubectx kubectx_$KUBECTX_VERSION_linux_x86_64.tar.gz && spinner &
-    elif [ "$os" == "darwin" ]; then
-        brew install kubectx
-    else
-        echo "Unsupported OS: $OS"
-    fi
+  case "$OSTYPE" in
+  linux*)
+    curl -fsSL "https://github.com/ahmetb/kubectx/releases/download/v0.9.1/kubectx_${KUBECTX_VERSION}_linux_x86_64.tar.gz" |
+      sudo tar -C /usr/local/bin --strip-components=1 -zxvf - "kubectx"
+    ;;
+  darwin*)
+    brew install kubectx
+    ;;
+  *)
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+    ;;
+  esac
+
+  echo "kubectx installed successfully"
 fi
 
 # Install Krew
 (
-    set -x; cd "$(mktemp -d)" &&
+  set -x
+  cd "$(mktemp -d)" &&
     OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
     ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
     KREW="krew-${OS}_${ARCH}" &&
     curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
     tar zxvf "${KREW}.tar.gz" &&
     ./"${KREW}" install krew &
-) > /dev/null 2>&1
+) >/dev/null 2>&1
 
-shell=$(basename "$SHELL")
-rc_file="$HOME/.${shell}rc"
+# Define the list of files to check
+readonly RC_FILES="$HOME/.*rc"
 
-# Append aliases to the correct rc file
+# Loop through each file and append aliases and update PATH
+for rc_file in $RC_FILES; do
+  if grep -q 'alias k=' "$rc_file" && grep -q 'alias kx=' "$rc_file" && grep -q 'PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' "$rc_file"; then
+    echo "Aliases and PATH already exist in $rc_file"
+  else
+    # Append aliases and update PATH
+    {
+      echo 'alias k=kubectl'
+      echo 'alias kx=kubectx'
+      echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"'
+    } >>"$rc_file"
 
-if grep -q 'alias k=' ~/.bashrc || grep -q 'alias k=' ~/.zshrc; then
-    echo "Alias k exists"
-else
-    echo "alias k=kubectl" >>"$(rc_file)"
-fi
+    echo "Updated $rc_file with aliases and PATH"
+  fi
 
-if grep -q 'alias kx=' ~/.bashrc || grep -q 'alias kx=' ~/.zshrc; then
-    echo "Alias kx exists"
-else
-    echo "alias kx=kubectx" >>"$rc_file"
-fi
-
-if grep -q 'PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' ~/.bashrc || grep -q 'PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' ~/.zshrc; then
-    echo "The PATH already exists in either ~/.bashrc or ~/.zshrc" > /dev/null 2>&1
-else
-    echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >>"$rc_file"
-fi
-
-# Source the file to make the changes take effect
-source "$rc_file" > /dev/null 2>&1
+  # Reload the file to make changes take effect
+  source "$rc_file" >/dev/null 2>&1
+done
 
 # Install krew plugins
 PLUGINS=(
-    "community-images"
-    "blame"
-    "tree"
-    "count"
-    "deprecations"
-    "datree"
-    "colorize-applied"
-    "explore"
+  "community-images"
+  "blame"
+  "tree"
+  "count"
+  "deprecations"
+  "datree"
+  "colorize-applied"
+  "explore"
 )
 
 for plugin in "${PLUGINS[@]}"; do
-    echo "Installing plugin: $plugin"
-    if ! kubectl krew list | grep -q "$plugin"; then
-    kubectl krew install "$plugin"
-    echo "Plugin $plugin installed successfully."
-    else
-    echo "Plugin $plugin is already installed."
-    fi
+  echo "Installing plugin: $plugin"
+  kubectl krew install "$plugin" || true
+  echo "Plugin $plugin installed successfully."
 done
